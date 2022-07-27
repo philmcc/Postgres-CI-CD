608ed15eb45cf3d794c2d2aa709fef4efc9ab238
Philip McClarence <philmcc@gmail.com>
2022-07-27 14:42:31 +0100

    params

--- sql/V1.0.2__First_Query.sql
+++ sql/V1.0.2__First_Query.sql
@@ -1,5 +1,5 @@
-PREPARE test_insert (detail TEXT) AS
+PREPARE test_insert (TEXT) AS
     INSERT INTO test (details) VALUES($1);
 
-PREPARE test_query (id int) AS
+PREPARE test_query (int) AS
    SELECT * FROM test where id = $1;
